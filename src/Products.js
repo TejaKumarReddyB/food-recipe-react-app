e95b5cd1eec985dcf63435bcc9beded97a92e4d0
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-10-21 12:11:11 +0530

    Update Products.js

--- src/Products.js
+++ src/Products.js
@@ -7,7 +7,7 @@ const Products = ({data}) => {
                 {data.map(data =>  
                 <div className="col-md-4">
                     <div class="card" style={{"width": "18rem"}}>
-                    <img class="card-img-top" src={data.recipe.image} alt="Card image cap" />
+                    <img class="card-img-top" src={data.recipe.image} alt="Card cap" />
                     <div class="card-body">
                     <center>
                         <h5 class="card-title">{data.recipe.label}</h5>
